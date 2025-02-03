31bc48f15952f91f619a057c8ea4fe1fbe62066f
Miсhael Belvedersky <mike.belveder@gmail.com>
2025-02-03 14:54:20 +0300

    FEAT: warnings on optional columns (user_srcid)

--- src/xmm_py_spec/utils.py
+++ src/xmm_py_spec/utils.py
@@ -1,6 +1,7 @@
 import csv
 from pathlib import Path
 from typing import List, Dict, Union
+import warnings
 
 
 def load_source_list(filepath: Union[str, Path]) -> List[Dict[str, str]]:
@@ -17,13 +18,21 @@ def load_source_list(filepath: Union[str, Path]) -> List[Dict[str, str]]:
         ValueError: If required columns are missing
     """
     required_cols = {'obs_id', 'src_num', 'srcid'}
+    optional_cols = {'user_srcid'}
 
     with open(filepath, 'r') as f:
         reader = csv.DictReader(f)
 
         if not required_cols.issubset(reader.fieldnames):
             print(set(reader.fieldnames))
-            missing = required_cols - set(reader.fieldnames)
-            raise ValueError(f"Missing required columns: {missing}")
+            missing_required = required_cols - set(reader.fieldnames)
+            raise ValueError(f"Missing required columns: {missing_required}")
+        if not optional_cols.issubset(reader.fieldnames):
+            print(set(reader.fieldnames))
+            missing_optional = optional_cols - set(reader.fieldnames)
+            warnings.warn(
+                f"Missing optional columns: {missing_optional}, continue...",
+                UserWarning
+            )
 
         return list(reader)
